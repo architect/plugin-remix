4617ddd8d48e7a91eedee1850e9917716c477e00
Taylor Beseda <tbeseda@gmail.com>
2022-03-11 11:20:18 -0700

    add config tests

--- test/plugin-remix-test.js
+++ test/plugin-remix-test.js
@@ -1,7 +1,102 @@
 const test = require('tape')
 const pluginRemix = require('../src')
+const { PLUGIN_DEFAULTS } = require('../src/constants')
+const {
+  createFinalRemixConfig,
+  createPluginConfigs,
+} = require('../src/util')
 
-test('test', function (t) {
+test('existential', (t) => {
   t.ok(pluginRemix, 'lib exists')
+  t.ok(createFinalRemixConfig, 'util.createFinalRemixConfig exists')
+  t.ok(createPluginConfigs, 'util.createPluginConfigs exists')
+  t.end()
+})
+
+test('plugin config generator', (t) => {
+  const arcProject = {
+    arc: {
+      remix: [],
+    },
+    cwd: './testProject'
+  }
+  const defaultExpectedOutput = {
+    pluginConfig: {
+      'mount': '/*',
+      'appDirectory': './app',
+      'buildDirectory': '.remix',
+      'serverDirectory': '.remix/server',
+      'serverHandler': PLUGIN_DEFAULTS.serverHandler,
+    },
+    initialRemixConfig: {
+      'appDirectory': './app',
+      'assetsBuildDirectory': './public/.remix/',
+      'cacheDirectory': '.remix/.cache/',
+      'publicPath': '/_static/.remix/',
+      'serverBuildPath': 'testProject/.remix/server/build.js',
+      'serverBuildTarget': 'arc'
+    }
+  }
+
+  t.deepEquals(createPluginConfigs(arcProject), defaultExpectedOutput, 'default config')
+
+  const verboseArcProject = {
+    arc: {
+      remix: [
+        [ 'app-directory', 'testing' ],
+        [ 'build-directory', '.testing' ],
+        [ 'mount', '/testing' ],
+        [ 'server-handler', './custom/testing.js' ],
+      ],
+    },
+    cwd: './testProject'
+  }
+  const verboseExpectedOutput = {
+    pluginConfig: {
+      'mount': '/testing',
+      'appDirectory': 'testing',
+      'buildDirectory': '.testing',
+      'serverDirectory': '.testing/server',
+      'serverHandler': './custom/testing.js',
+    },
+    initialRemixConfig: {
+      'appDirectory': 'testing',
+      'assetsBuildDirectory': './public/.testing/',
+      'cacheDirectory': '.testing/.cache/',
+      'publicPath': '/_static/.testing/',
+      'serverBuildPath': 'testProject/.testing/server/build.js',
+      'serverBuildTarget': 'arc'
+    }
+  }
+
+  t.deepEquals(createPluginConfigs(verboseArcProject), verboseExpectedOutput, 'verbose config')
+
+  t.end()
+})
+
+test('remix config generator', async (t) => {
+  const arcInv = {
+    _project: {
+      arc: {
+        remix: [],
+      },
+      cwd: './test/testProject'
+    }
+  }
+  const expectedOutput = {
+    appDirectory: './app',
+    cacheDirectory: '.remix/.cache/',
+    assetsBuildDirectory: './public/.remix/',
+    publicPath: '/_static/.remix/',
+    serverBuildPath: 'test/testProject/.remix/server/build.js',
+    // serverMode: 'production',
+    serverBuildTarget: 'arc',
+  }
+  const actualOutput = await createFinalRemixConfig(arcInv)
+
+  for (const key in expectedOutput) {
+    t.equals(actualOutput[key], expectedOutput[key], key)
+  }
+
   t.end()
 })
---
+++ test/testProject/app/root.jsx
@@ -0,0 +1,24 @@
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix';
+
+export const meta = () => {
+  return { title: 'New Remix App' };
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
